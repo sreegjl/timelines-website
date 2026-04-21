bf01a5da6ccc36beb670740493ad1877436fcbab
Sree Gajula <realpopnerd@gmail.com>
2026-04-20 17:12:59 -0700

    update downloads/changelog

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -10,13 +10,13 @@ function Download() {
         <div className="download-card">
           <img src={`${import.meta.env.BASE_URL}icon.png`} alt="Timelines app icon" className="download-card-icon" />
           <h3>Windows</h3>
-          <a href="https://github.com/sreegjl/timelines/releases/download/v0.2.0-alpha.1/Timelines-0.2.0-alpha.1-Setup.exe" className="btn btn-primary">Download for Windows</a>
+          <a href="https://github.com/sreegjl/timelines/releases/download/v0.4.0-alpha.1/Timelines-0.4.0-alpha.1-Setup.exe" className="btn btn-primary">Download for Windows</a>
           <span className="download-meta">.exe &middot; 64-bit</span>
         </div>
         <div className="download-card">
           <img src={`${import.meta.env.BASE_URL}icon.png`} alt="Timelines app icon" className="download-card-icon" />
           <h3>macOS</h3>
-          <a href="https://github.com/sreegjl/timelines/releases/download/v0.2.0-alpha.1/Timelines-0.2.0-alpha.1-Setup.dmg" className="btn btn-primary">Download for Mac</a>
+          <a href="https://github.com/sreegjl/timelines/releases/download/v0.4.0-alpha.1/Timelines-0.4.0-alpha.1-Setup.dmg" className="btn btn-primary">Download for Mac</a>
           <span className="download-meta">.dmg &middot; Universal</span>
         </div>
       </div>
